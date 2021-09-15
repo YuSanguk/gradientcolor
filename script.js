438a48ab93c94d842186acb6b69ee1e4c43f234d
YuSanguk <75666980+YuSanguk@users.noreply.github.com>
2021-09-15 10:43:31 +0900

    add palette
    add palette function

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const checkbox2 = document.querySelector("#checkbox2");
 const color1 = document.querySelector("#color1");
 const color2 = document.querySelector("#color2");
 const background = document.querySelector(".container");
+const palette = document.querySelector("#palette");
 
+let color_value = [
+  ["#ffffff", "#000000"],
+  ["#ffffff", "#000000"],
+  ["#ffffff", "#000000"],
+  ["#ffffff", "#000000"],
+];
+
+// color change
 function create_color() {
   const letters = "0123456789ABCDEF";
   let color = "#";
@@ -14,6 +23,7 @@ function create_color() {
   return color;
 }
 
+// when chlick button
 button.addEventListener("click", () => {
   let color;
   if (checkbox1.checked === false) {
@@ -25,18 +35,32 @@ button.addEventListener("click", () => {
     color = create_color();
     color2.innerHTML = color;
   }
-
+  const palette_number = palette.value[7];
   const c1 = color1.innerHTML;
   const c2 = color2.innerHTML;
+  color_value[palette_number - 1][0] = c1;
+  color_value[palette_number - 1][1] = c2;
   const text = "linear-gradient(90deg," + c1 + "," + c2 + ")";
   background.style.background = text;
 });
 
+// copy function
 function copy(element) {
   var $temp = $("<input>");
   $("body").append($temp);
   $temp.val($(element).text()).select();
   document.execCommand("copy");
   $temp.remove();
-  alert('Copied!');
+  alert("copied!");
 }
+
+// change select
+const palette_control = () => {
+  const palette_number = palette.value[7];
+  color2.innerHTML = color_value[palette_number - 1][0];
+  color1.innerHTML = color_value[palette_number - 1][1];
+  const c1 = color1.innerHTML;
+  const c2 = color2.innerHTML;
+  const text = "linear-gradient(90deg," + c1 + "," + c2 + ")";
+  background.style.background = text;
+};
